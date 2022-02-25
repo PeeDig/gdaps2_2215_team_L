7a1f537f35a446419b5768f1f529b6b71a894ea5
aidanloucks <ajl5556@g.rit.edu>
2022-02-25 17:48:20 -0500

    hit/hurt box classes

---
+++ game/Team_Majx_Game/Team_Majx_Game/HurtBox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Team_Majx_Game
+{
+    // Basic information for the
+    // hurtbox of projectiles / damaging moves
+    class HurtBox
+    {
+    }
+}
